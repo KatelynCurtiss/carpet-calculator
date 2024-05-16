80d54bcee273cc25b3533d3d1da6cd2d3868cce6
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2024-05-16 09:18:46 -0400

    update

--- main.js
+++ main.js
@@ -3,12 +3,15 @@
 // Simple Addition Calculator 
 
 document.getElementById("btn").addEventListener("click, btnClicked");
+
+function btnClicked() {
 // Input 
-let num1 = document.getElementById("num1-in").value;
-let num2 = document.getElementById("num2-in").value;
+let num1 = +document.getElementById("num1-in").value;
+let num2 = +document.getElementById("num2-in").value;
 
 // Process
 let total = num1 + num2;
 
 // Output
-document.getElementsById("")
+document.getElementsById("output").innerHTML = total; 
+}
